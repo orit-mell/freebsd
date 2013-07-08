fa0c5ddfbe84f4c6c7898cd99cbab511f06cff5d
Orit Moskovich <oritm@mellanox.com>
2013-07-08 14:43:43 +0300

    mlx4_ib integration: Comment out mlx4_ib_mmap() and revert ib_umem.h to original 1.5.3 Isilon header
    Issue:196239
    
    Change-Id: I9adcd3aeac5d887a5fb18a297d920dbb77636866
    Signed-off-by: Orit Moskovich <oritm@mellanox.com>

--- sys/ofed/include/rdma/ib_umem.h
+++ sys/ofed/include/rdma/ib_umem.h
@@ -48,41 +48,14 @@ struct ib_umem {
 	int                     writable;
 	int                     hugetlb;
 	struct list_head	chunk_list;
+#ifdef __linux__
 	struct work_struct	work;
 	struct mm_struct       *mm;
+#else
+	unsigned long		start;
+#endif
 	unsigned long		diff;
 };
-
-/* contiguous memory structure */
-struct ib_cmem {
-
-	struct ib_ucontext     *context;
-	size_t			length;
-	/* Link list of contiguous blocks being part of that cmem  */
-	struct list_head ib_cmem_block;
-
-	/* Order of cmem block,  2^ block_order will equal number
-	     of physical pages per block
-	*/
-	unsigned long    block_order;
-	/* Refernce counter for that memory area
-	  - When value became 0 pages will be returned to the kernel.
-	*/
-	struct kref refcount;
-};
-
-
-struct ib_cmem_block {
-
-	struct list_head	list;
-	/* page will point to the page struct of the head page
-	    in the current compound page.
-	    block order is saved once as part of ib_cmem.
-	*/
-	struct page            *page;
-};
-
-
 
 struct ib_umem_chunk {
 	struct list_head	list;
@@ -92,34 +65,9 @@ struct ib_umem_chunk {
 	struct scatterlist      page_list[0];
 };
 
-#ifdef CONFIG_INFINIBAND_USER_MEM
-
 struct ib_umem *ib_umem_get(struct ib_ucontext *context, unsigned long addr,
 			    size_t size, int access, int dmasync);
 void ib_umem_release(struct ib_umem *umem);
 int ib_umem_page_count(struct ib_umem *umem);
-
-int ib_cmem_map_contiguous_pages_to_vma(struct ib_cmem *ib_cmem,
-	struct vm_area_struct *vma);
-struct ib_cmem *ib_cmem_alloc_contiguous_pages(struct ib_ucontext *context,
-				unsigned long total_size,
-				unsigned long page_size_order);
-void ib_cmem_release_contiguous_pages(struct ib_cmem *cmem);
-int ib_umem_map_to_vma(struct ib_umem *umem,
-				struct vm_area_struct *vma);
-
-#else /* CONFIG_INFINIBAND_USER_MEM */
-
-#include <linux/err.h>
-
-static inline struct ib_umem *ib_umem_get(struct ib_ucontext *context,
-					  unsigned long addr, size_t size,
-					  int access, int dmasync) {
-	return ERR_PTR(-EINVAL);
-}
-static inline void ib_umem_release(struct ib_umem *umem) { }
-static inline int ib_umem_page_count(struct ib_umem *umem) { return 0; }
-
-#endif /* CONFIG_INFINIBAND_USER_MEM */
 
 #endif /* IB_UMEM_H */
